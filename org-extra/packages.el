a57cf7fd69984382210fce95048404e80ecfbc9c
orab <orabfy@gmail.com>
2016-07-01 11:08:46 +0800

    add org agenda archive skip rules

--- org-extra/packages.el
+++ org-extra/packages.el
@@ -191,7 +191,12 @@ Each entry is either:
                  nil))))
 
   (setq org-agenda-cmp-user-defined 'orabfy/agenda-sort)
-  )
+
+  (setq org-archive-mark-done nil)
+  (setq org-archive-location "%s_archive::* Archive")
+  ;; Include agenda archive files when searching for things
+  (setq org-agenda-text-search-extra-files (quote (agenda-archives)))
+ )
 
 (defun org-extra/init-org-mobile-sync ()
   (use-package org-mobile-sync
@@ -665,5 +670,31 @@ Late deadlines first, then scheduled, then non-late deadlines"
 
 (defun orabfy/is-scheduled-late (date-str)
   (string-match "Sched\.\\(.*\\)x:" date-str))
+
+(defun orabfy/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    ;; Consider only tasks with done todo headings as archivable candidates
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (member (org-get-todo-state) org-todo-keywords-1)
+          (if (member (org-get-todo-state) org-done-keywords)
+              (let* ((daynr (string-to-number (format-time-string "%d" (current-time))))
+                     (a-month-ago (* 60 60 24 (+ daynr 1)))
+                     (last-month (format-time-string
+                                  "%Y-%m-"
+                                  (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                     (this-month (format-time-string "%Y-%m-" (current-time)))
+                     (subtree-is-current
+                      (save-excursion
+                        (forward-line 1)
+                        (and (< (point) subtree-end)
+                             (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+                (if subtree-is-current
+                    subtree-end ; Has a date in this month or last month, skip it
+                  nil))  ; available to archive
+            (or subtree-end (point-max)))
+        next-headline))))
 
 ;;; packages.el ends here
